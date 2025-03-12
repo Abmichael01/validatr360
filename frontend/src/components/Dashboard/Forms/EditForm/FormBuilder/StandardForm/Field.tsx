1302bdb125791522edcf633f72fc5edbc410972e
Urkel <abmichael109@gmail.com>
2025-03-12 20:06:45 +0100

    made new changes

--- frontend/src/components/Dashboard/Forms/EditForm/FormBuilder/StandardForm/Field.tsx
+++ frontend/src/components/Dashboard/Forms/EditForm/FormBuilder/StandardForm/Field.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-
-export const Field: React.FC = () => {
+const Field: React.FC = () => {
   return (
     <div>
       The form builder will be here
     </div>
   )
 }
+
+export default Field;
